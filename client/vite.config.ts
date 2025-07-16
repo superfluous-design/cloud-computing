da176f87fb2375ddb2c2eb7ea6119b39dfeb969b
FroelingDev <alonsofroelingn@wit.edu>
2025-07-16 19:46:35 -0400

    docker vite conf

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "node:path";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": resolve(fileURLToPath(new URL(".", import.meta.url)), "./src"),
     },
   },
   // @ts-ignore
